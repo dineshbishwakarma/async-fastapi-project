1967271e016496d00c6dcacf24f37164c44f211b
dineshbishwakarma <ghatanidinesh56@gmail.com>
2023-12-14 16:53:48 -0600

    Add User table to database

--- models.py
+++ models.py
@@ -33,4 +33,12 @@ class Post(Base):
     content: Mapped[str] = mapped_column(Text, nullable=False)
     comments: Mapped[list[Comment]] = relationship("Comment", cascade="all, delete")
 
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True,autoincrement=True)
+    email: Mapped[str] = mapped_column(String(1024), index=True, unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(1024), nullable=False)
     
+
+
